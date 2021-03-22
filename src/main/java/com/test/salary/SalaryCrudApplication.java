a4fd6f1a5d096a1b7608a1e277197b920462d36e
Wilians Perez <willpe44@gmail.com>
2021-03-21 21:38:26 -0500

    first commit

---
+++ src/main/java/com/test/salary/SalaryCrudApplication.java
@@ -0,0 +1,13 @@
+package com.test.salary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalaryCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SalaryCrudApplication.class, args);
+    }
+
+}
